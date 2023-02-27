98def2025a4f78e2743d9b11a202e15337d74fd2
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2023-02-27 10:33:55 +0300

    Added components

--- client/my-app-frontend/src/App.js
+++ client/my-app-frontend/src/App.js
@@ -1,9 +1,56 @@
 
 
-function App() {
+import React, {useState} from "react";
+import { Routes, Route } from "react-router-dom";
+import ProductContainer from "./components/ProductContainer";
+import NavBar from "./components/NavBar";
+import AddProduct from "./components/AddProduct";
+import  About  from "./components/About";
+import Footer from "./components/Footer";
+import AddToCart from "./components/AddToCart";
+
+
+
+function App() { 
+    const [isInCart, setIsInCart] = useState([])
+
+    const handleClick = (product) => {
+      if (isInCart.indexOf(product) !== -1) return;
+      setIsInCart([...isInCart, product]);
+    };
+
+    const handleChange = (product, d) => {
+      const ind = isInCart.indexOf(product);
+      const arr = [...isInCart];
+      if (arr[ind].amount === undefined || isNaN(arr[ind].amount)) arr[ind].amount = 0;
+      arr[ind].amount += d;
+      if (arr[ind].amount === 0) arr[ind].amount = 1;
+      setIsInCart([...arr]);
+    };
+ 
+
   return (
-    <div >
-      <h1>Heyyyy</h1>
+    <div>
+     
+      <NavBar num={isInCart.length}/>
+      
+
+      <Routes>
+        <Route path="/" element = {<ProductContainer handleClick = {handleClick}/>} />
+        <Route path="addproduct" element ={ <AddProduct   />}/>               
+        <Route path="about" element ={ <About />}/>
+        <Route path="addtocart" element = { <AddToCart setIsInCart={setIsInCart} isInCart={isInCart} handleChange={handleChange}/>} />
+    
+        
+       
+        
+       
+      </Routes>
+
+      <Footer />
+      
+     
+      
     </div>
   );
 }
